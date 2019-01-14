80522b9db43fa65d85dfa7d918a2eea598bab6fe
Bisma Abdurachman Tauhid <batdps@gmail..com>
2019-01-15 03:15:12 +0700

    List Quiz

---
+++ Assets/Scripts/CoreGameplay/QuizManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class QuizManager : MonoBehaviour
+{
+	public Text QuestionContent;
+	public Text Answer1Content;
+	public Text Answer2Content;
+	public int IdQuestionPopup;
+
+	public List <Quiz> ListQuiz = new List<Quiz>();
+
+	private void Awake()
+	{
+		for (int i = 0; i < ListQuiz.Count; i++) 
+		{
+			
+			if (IdQuestionPopup==ListQuiz[i].Id) 
+			{
+				QuestionContent.text = ListQuiz[i].Question;
+				Answer1Content.text = ListQuiz [i].ListAnswer [0].AnswerContent;
+				Answer2Content.text = ListQuiz [i].ListAnswer [1].AnswerContent;
+			}
+		}
+
+		/*
+		foreach (var quizdata in ListQuiz) 
+		{
+			if (IdQuestionPopup==quizdata.Id) 
+			{
+				QuestionContent.text = ListQuiz[quizdata.Id].Question;
+				Answer1Content.text = ListQuiz [quizdata.Id].ListAnswer [0].AnswerContent;
+				Answer2Content.text = ListQuiz [quizdata.Id].ListAnswer [1].AnswerContent;
+			}
+		}
+		*/
+	}
+}
---
+++ Assets/Scripts/Events/GameplayEvents.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class Answer
+{
+	public string AnswerContent;
+	public bool IsRight;
+
+}
+[System.Serializable]
+public class Quiz
+{
+	public int Id;
+	public string Question;
+	public List <Answer> ListAnswer = new List<Answer>(); 
+}
