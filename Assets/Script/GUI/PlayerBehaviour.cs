e038e219a71258d8a380d7ab8c8d24875050b0e3
Bisma <batdps@gmail.com>
2019-01-14 20:19:21 +0700

    Player Movement

--- Assets/Script/GUI/PlayerBehaviour.cs
+++ Assets/Script/GUI/PlayerBehaviour.cs
@@ -6,17 +6,47 @@ public class PlayerBehaviour : MonoBehaviour
 {
     [SerializeField]
     float speed; 
-
-    Rigidbody2D riggidBody;      
+	bool move=false;
+	Vector2 mousePos2d;
+	Vector3 mousePos;
+    Rigidbody2D rigidBody;      
 
     // Use this for initialization
     void Start()
     {
-        riggidBody = GetComponent<Rigidbody2D>();
+        rigidBody = GetComponent<Rigidbody2D>();
     }
     
     void Update()
     {
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Debug.Log(mousePos);
+			mousePos2d = new Vector2(mousePos.x, mousePos.y);
+			mousePos.z = transform.position.z;
+			RaycastHit2D hit = Physics2D.Raycast(mousePos2d, Vector2.zero);
+			if (hit.transform == null) {
+				return;
+			}
+			if ((hit.transform.gameObject.tag=="People"))
+			{
+				if (move==false) 
+				{
+					move = true;
+				}
+			}
+			else
+			{
+				return; //Fail
+			}
+
+		}
+		if (move==true) 
+		{
+			transform.position = Vector3.MoveTowards (transform.position, mousePos, speed * Time.deltaTime);
+			//move = false;
+		}
 
     }
 }
